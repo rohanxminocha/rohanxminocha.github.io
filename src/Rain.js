ef80829e4155cb7ad54bbebbf7fcef037e5ddc78
Rohan Minocha <rohan.minocha2002@gmail.com>
2024-05-05 20:06:50 -0400

    Add file via upload

--- src/Rain.js
+++ src/Rain.js
@@ -9,22 +9,27 @@ const Rain = ({ color = 'black' }) => {
       const drop = {
         id: Date.now(),
         x: Math.random() * window.innerWidth,
-        y: -10,
+        y: -Math.random() * window.innerHeight,
         speed: Math.random() * 5 + 2,
+        height: Math.random() * 20 + 10,
       };
-      setDrops(prevDrops => [...prevDrops, drop])
+      setDrops(prevDrops => [...prevDrops, drop]);
     };
 
+    const maxDrops = 50;
+
     const fallDrops = setInterval(() => {
-      createDrop();
+      if (drops.length < maxDrops) {
+        createDrop();
+      }
       setDrops(prevDrops =>
-        prevDrops.filter(drop => drop.y < window.innerHeight)
+        prevDrops.filter(drop => drop.y < window.innerHeight + drop.height)
           .map(drop => ({ ...drop, y: drop.y + drop.speed }))
       );
-    }, 100);
+    }, 200);
 
     return () => clearInterval(fallDrops);
-  }, []);
+  }, [drops]);
 
   return (
     <div className="rain-container">
--- src/index.js
+++ src/index.js
@@ -4,19 +4,25 @@ import ReactGA from 'react-ga';
 import Snowfall from 'react-snowfall';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Rain from './Rain'
+import Rain from './Rain';
+import Weather from './Weather';
 import App from './App';
 
 ReactGA.initialize('UA-145825675-1');
 ReactGA.pageview('/');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <div style={{ background: '#282c34', position: 'relative' }}>
-      <App />
-      <Snowfall color='#f0eae4' snowflakeCount={200} radius={[1.0, 10.0]} />
-      <Rain color='#5c4033'/>
-    </div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const WeatherAnimation = () => {
+  const weather = Weather();
+
+  return (
+    <React.StrictMode>
+      <div>
+        <App />
+        {weather === 'Snow' && <Snowfall color='#f0eae4' snowflakeCount={100} radius={[1.0, 10.0]} />}
+        {weather === 'Rain' && <Rain color='#5c4033' />}
+      </div>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<WeatherAnimation />, document.getElementById('root'));
