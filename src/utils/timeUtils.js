51123acdfd94756f5fa43e985cf864d07449b0ae
Rohan Minocha <rohan.minocha2002@gmail.com>
2024-06-15 00:43:28 -0400

    major upgrade

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -1,22 +1,20 @@
-// export const isEasterEggTime = () => {
-//     const now = new Date();
-//     const day = now.getUTCDay();
-//     const hour = now.getUTCHours();
-
-//     // Convert to Eastern Time (UTC-4)
-//     const easternOffset = -4 * 60 * 60 * 1000;
-//     const easternTime = new Date(now.getTime() + easternOffset);
-
-//     const easternDay = easternTime.getDay();
-//     const easternHour = easternTime.getHours();
-
-//     const isWeekday = easternDay >= 2 && easternDay <= 4; // Tuesday to Thursday
-//     const isMorningTime = easternHour === 11;
-//     const isAfternoonTime = easternHour >= 16 && easternHour < 18;
-
-//     return isWeekday && (isMorningTime || isAfternoonTime);
-// };
-
 export const isEasterEggTime = () => {
-    return true;
+    const now = new Date();
+
+    const easternOffset = -4 * 60 * 60 * 1000;
+    let easternTime = new Date(now.getTime() + easternOffset);
+
+    const isDST = now.getTimezoneOffset() < easternTime.getTimezoneOffset();
+    if (isDST) {
+        easternTime = new Date(easternTime.getTime() - 60 * 60 * 1000);
+    }
+
+    const easternDay = easternTime.getDay();
+    const easternHour = easternTime.getHours();
+
+    const isWeekday = easternDay >= 2 && easternDay <= 4;
+    const isMorningTime = easternHour === 11;
+    const isAfternoonTime = easternHour >= 16 && easternHour < 18;
+
+    return isWeekday && (isMorningTime || isAfternoonTime);
 };
